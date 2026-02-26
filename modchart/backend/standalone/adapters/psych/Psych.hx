2e8dc6fb6db3845b38d8ebbbf5bd2a9e4bb4d135
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-02-25 20:31:00 -0800

    fixing the original of the paths to the game

--- modchart/backend/standalone/adapters/psych/Psych.hx
+++ modchart/backend/standalone/adapters/psych/Psych.hx
@@ -3,10 +3,10 @@ package modchart.backend.standalone.adapters.psych;
 #if (FM_ENGINE_VERSION == "1.0" || FM_ENGINE_VERSION == "0.7")
 import backend.ClientPrefs;
 import backend.Conductor;
-import game.funkin.objects.Note;
-import game.funkin.objects.NoteSplash;
-import game.funkin.objects.StrumNote as Strum;
-import game.funkin.PlayState;
+import games.funkin.objects.Note;
+import games.funkin.objects.NoteSplash;
+import games.funkin.objects.StrumNote as Strum;
+import games.funkin.PlayState;
 #else
 import ClientPrefs;
 import Conductor;
